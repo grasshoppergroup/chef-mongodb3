992a7fc2c032636b7aef4aa1c32f361f7bcdc0ef
BruceCutler <bicutler10@gmail.com>
2017-08-15 13:09:31 -0400

    GDE-1161 Updating dependencies to allow for berks upload

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures mongodb3'
 long_description 'Installs/Configures mongodb3'
 
-version '0.0.1'
+version '5.4.0'
 chef_version '>= 12.7' if respond_to?(:chef_version)
 
 supports 'ubuntu', '>= 12.04'
@@ -16,7 +16,7 @@ supports 'centos', '= 7.2'
 supports 'oracle', '= 6.6'
 supports 'amazon'
 
-depends 'apt'
-depends 'yum'
+depends 'apt', '~> 6.1'
+depends 'yum', '~> 5.0'
 depends 'user'
 depends 'runit', '~> 1.7.0'
